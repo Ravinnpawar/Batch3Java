e3dd9d51f67a9e448741a06b69acfff168929fff
Ashwini <ashwinib7293@gmail.com>
2022-08-11 11:26:54 +0530

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+//public class <ClassName> {
+// ------------
+// -your code to run or methods
+// ------------
+// } <<-------------------------------------Syntax for class
+//public-- Access specifier
+//class- keyword to crerate class
+//classname- to identity
+//main method- entry point
+//static - it is required to load at compile time only.
+//void - return type- does not return anything.
+//String[] args
+//System- Class
+//out- object of printstream in system class
+//println - IT is a method inside printstream class
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
